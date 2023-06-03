6dc7195fdf3e38994b0156a615274774515c84b5
JavierPintoVas <javierpintovas@gmail.com>
2023-06-03 08:45:50 -0500

    Adding funtionRecognition

--- index.js
+++ index.js
@@ -74,6 +74,9 @@ function guardarConfiguracion() {
   if (puertasAbiertas) {
     funtionDoors(puertasAbiertas);
   }
+  if (reconocimientoActivado) {
+    funtionRecognition(reconocimientoActivado);
+  }
   window.onload = function () {
     var panel = document.getElementById("panel");
     panel.style.display = "flex";
@@ -147,146 +150,151 @@ const stopCamera = () => {
 };
 
 // Request permission to use the microphone
-navigator.mediaDevices
-  .getUserMedia({ audio: true })
-  .then(function (stream) {
-    try {
-      // Create a new SpeechRecognition object
+var funtionRecognition = (reconocimientoActivado) => {
+  if (reconocimientoActivado) {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true })
+      .then(function (stream) {
+        try {
+          // Create a new SpeechRecognition object
 
-      const recognition = new window.webkitSpeechRecognition();
-      console.log(recognition);
-      // Configure the recognition object
-      recognition.lang = "es-US";
-      recognition.continuous = true;
+          const recognition = new window.webkitSpeechRecognition();
+          console.log(recognition);
+          // Configure the recognition object
+          recognition.lang = "es-US";
+          recognition.continuous = true;
 
-      // Handle recognition events
-      recognition.addEventListener("result", function (event) {
-        console.log(event);
-        // TODO Quitar caracteres especiales, tildes!
-        const text =
-          event.results[event.results.length - 1][0].transcript.toLowerCase(); // Hola Javier, prende la luz por favor
-        // Si text incluye "prende la luz"
-        // Entonces prende la luz
-        let actionType = "";
-        let action = "";
-        let elementId = "";
+          // Handle recognition events
+          recognition.addEventListener("result", function (event) {
+            console.log(event);
+            // TODO Quitar caracteres especiales, tildes!
+            const text =
+              event.results[
+                event.results.length - 1
+              ][0].transcript.toLowerCase(); // Hola Javier, prende la luz por favor
+            // Si text incluye "prende la luz"
+            // Entonces prende la luz
+            let actionType = "";
+            let action = "";
+            let elementId = "";
 
-        console.log(text);
+            console.log(text);
 
-        const activationWords =
-          /(enciende|prende|prenda|ilumina|encender|activa|activar)/i;
+            const activationWords =
+              /(enciende|prende|prenda|ilumina|encender|activa|activar)/i;
 
-        const deactivationWords =
-          /(apaga|apagar|oscurece|oscurecer|desactiva|desactivar)/i;
+            const deactivationWords =
+              /(apaga|apagar|oscurece|oscurecer|desactiva|desactivar)/i;
 
-        if (deactivationWords.test(text)) {
-          actionType = "light";
-          action = "remove";
-        } else if (activationWords.test(text)) {
-          actionType = "light";
-          action = "add";
-        } else if (text.includes("abre") || text.includes("abrir")) {
-          actionType = "doorsAndWindows";
-          action = "add";
-        } else if (text.includes("cierra") || text.includes("cerrar")) {
-          actionType = "doorsAndWindows";
-          action = "remove";
-        }
-
-        if (action != null) {
-          if (text.includes("habitación")) {
-            if (text.includes("uno") || text.includes("principal")) {
-              elementId = "mainBedroom";
-            } else if (text.includes("dos") || text.includes("segunda")) {
-              elementId = "bedroomTwo";
-            } else if (text.includes("tres") || text.includes("tercera")) {
-              elementId = "bedroomThree";
+            if (deactivationWords.test(text)) {
+              actionType = "light";
+              action = "remove";
+            } else if (activationWords.test(text)) {
+              actionType = "light";
+              action = "add";
+            } else if (text.includes("abre") || text.includes("abrir")) {
+              actionType = "doorsAndWindows";
+              action = "add";
+            } else if (text.includes("cierra") || text.includes("cerrar")) {
+              actionType = "doorsAndWindows";
+              action = "remove";
             }
-          } else if (text.includes("baño")) {
-            if (text.includes("uno") || text.includes("principal")) {
-              elementId = "bathroomBedroomOne";
-            } else if (
-              text.includes("dos") ||
-              text.includes("social") ||
-              text.includes("pasillo")
-            ) {
-              elementId = "bathroomBedroomTwo";
-            }
-          } else if (text.includes("cocina")) {
-            elementId = "kitchen";
-          } else if (text.includes("sala")) {
-            elementId = "livingRoom";
-          } else if (text.includes("pasillo")) {
-            elementId = "hall";
-          } else if (text.includes("lavandería")) {
-            elementId = "laundry";
-          } else if (text.includes("balcón")) {
-            elementId = "balcony";
-          } else if (text.includes("cámara")) {
-            if (action === "add") {
-              startCamera();
-            } else if (action === "remove") {
-              stopCamera();
-            }
-          } else if (text.includes("todas las luces")) {
-            Object.keys(rooms).forEach((roomId) => {
-              rooms[roomId].classList[action]("lights-on");
-            });
-          } else if (text.includes("puerta principal")) {
-            elementId = "principalDoor";
-          } else if (text.includes("todas las puertas")) {
-            Object.keys(doorsAndWindows).forEach((roomId) => {
-              if (roomId.includes("Door")) {
-                doorsAndWindows[roomId].classList[action]("open");
+
+            if (action != null) {
+              if (text.includes("habitación")) {
+                if (text.includes("uno") || text.includes("principal")) {
+                  elementId = "mainBedroom";
+                } else if (text.includes("dos") || text.includes("segunda")) {
+                  elementId = "bedroomTwo";
+                } else if (text.includes("tres") || text.includes("tercera")) {
+                  elementId = "bedroomThree";
+                }
+              } else if (text.includes("baño")) {
+                if (text.includes("uno") || text.includes("principal")) {
+                  elementId = "bathroomBedroomOne";
+                } else if (
+                  text.includes("dos") ||
+                  text.includes("social") ||
+                  text.includes("pasillo")
+                ) {
+                  elementId = "bathroomBedroomTwo";
+                }
+              } else if (text.includes("cocina")) {
+                elementId = "kitchen";
+              } else if (text.includes("sala")) {
+                elementId = "livingRoom";
+              } else if (text.includes("pasillo")) {
+                elementId = "hall";
+              } else if (text.includes("lavandería")) {
+                elementId = "laundry";
+              } else if (text.includes("balcón")) {
+                elementId = "balcony";
+              } else if (text.includes("cámara")) {
+                if (action === "add") {
+                  startCamera();
+                } else if (action === "remove") {
+                  stopCamera();
+                }
+              } else if (text.includes("todas las luces")) {
+                Object.keys(rooms).forEach((roomId) => {
+                  rooms[roomId].classList[action]("lights-on");
+                });
+              } else if (text.includes("puerta principal")) {
+                elementId = "principalDoor";
+              } else if (text.includes("todas las puertas")) {
+                Object.keys(doorsAndWindows).forEach((roomId) => {
+                  if (roomId.includes("Door")) {
+                    doorsAndWindows[roomId].classList[action]("open");
+                  }
+                });
+              } else if (text.includes("todas las ventanas")) {
+                Object.keys(doorsAndWindows).forEach((roomId) => {
+                  if (roomId.includes("Window")) {
+                    doorsAndWindows[roomId].classList[action]("open");
+                  }
+                });
               }
-            });
-          } else if (text.includes("todas las ventanas")) {
-            Object.keys(doorsAndWindows).forEach((roomId) => {
-              if (roomId.includes("Window")) {
-                doorsAndWindows[roomId].classList[action]("open");
+
+              if (elementId) {
+                if (text.includes("puerta principal")) {
+                  doorsAndWindows[elementId].classList[action]("open");
+                } else if (text.includes("puerta")) {
+                  elementId += "Door";
+                } else if (text.includes("ventana")) {
+                  elementId += "Window";
+                }
               }
-            });
-          }
-
-          if (elementId) {
-            if (text.includes("puerta principal")) {
-              doorsAndWindows[elementId].classList[action]("open");
-            } else if (text.includes("puerta")) {
-              elementId += "Door";
-            } else if (text.includes("ventana")) {
-              elementId += "Window";
             }
-          }
-        }
 
-        console.log(action);
-        console.log(actionType);
-        console.log(elementId);
+            console.log(action);
+            console.log(actionType);
+            console.log(elementId);
 
-        if (action && actionType && elementId) {
-          if (actionType === "light") {
-            rooms[elementId].classList[action]("lights-on");
-          } else if (actionType === "doorsAndWindows") {
-            doorsAndWindows[elementId].classList[action]("open");
-          }
-        }
+            if (action && actionType && elementId) {
+              if (actionType === "light") {
+                rooms[elementId].classList[action]("lights-on");
+              } else if (actionType === "doorsAndWindows") {
+                doorsAndWindows[elementId].classList[action]("open");
+              }
+            }
 
-        console.log("Recognized text:", text);
+            console.log("Recognized text:", text);
 
-        // Do something with the recognized text, such as toggling lights on and off
+            // Do something with the recognized text, such as toggling lights on and off
+          });
+
+          recognition.start();
+
+          // Get the start and stop buttons
+          const startButton = document.getElementById("start-button");
+          const stopButton = document.getElementById("stop-button");
+        } catch (error) {}
+      })
+      .catch(function (error) {
+        console.error("Error getting user media:", error);
       });
-
-      recognition.start();
-
-      // Get the start and stop buttons
-      const startButton = document.getElementById("start-button");
-      const stopButton = document.getElementById("stop-button");
-    } catch (error) {}
-  })
-  .catch(function (error) {
-    console.error("Error getting user media:", error);
-  });
-
+  }
+};
 const saveName = () => {
   const input = document.getElementById("MyNameIs");
   const name = input.value;

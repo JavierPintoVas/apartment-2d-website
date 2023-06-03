ba180a4ebb043dffb7d6c5adf74c1605cc1ca7a8
JavierPintoVas <javierpintovas@gmail.com>
2023-06-03 08:34:59 -0500

    Adding checkbox funtional

--- index.js
+++ index.js
@@ -26,6 +26,12 @@ const doorsAndWindowsIds = [
   "livingRoomWindow",
 ];
 
+var nombreInput = document.getElementById("nombreInput");
+var lucesCheckbox = document.getElementById("lucesCheckbox");
+var puertasCheckbox = document.getElementById("puertasCheckbox");
+var cameraCheckbox = document.getElementById("cameraCheckbox");
+var reconocimientoCheckbox = document.getElementById("reconocimientoCheckbox");
+
 const rooms = {};
 const doorsAndWindows = {};
 
@@ -40,59 +46,96 @@ doorsAndWindowsIds.forEach((doorsAndWindowsId) => {
 
 let cameraStatus = false;
 
+function guardarConfiguracion() {
+  var nombreUsuario = nombreInput.value;
+  var lucesActivadas = lucesCheckbox.checked;
+  var puertasAbiertas = puertasCheckbox.checked;
+  var reconocimientoActivado = reconocimientoCheckbox.checked;
+  var cameraActivada = cameraCheckbox.checked;
+
+  // Aquí puedes hacer lo que necesites con las configuraciones guardadas y el nombre del usuario
+
+  var panel = document.getElementById("panel");
+  panel.style.display = "none";
+
+  // Reproducir el nombre del usuario en forma de mensaje de voz
+  var mensajeVoz = new SpeechSynthesisUtterance();
+  mensajeVoz.text =
+    "Hola " + nombreUsuario + ", tu configuración ha sido guardada.";
+  speechSynthesis.speak(mensajeVoz);
+
+  if (cameraActivada) {
+    // Resto del código relacionado con la cámara
+    startCamera(cameraActivada);
+  }
+  if (lucesActivadas) {
+    funtionLight(lucesActivadas);
+  }
+  if (puertasAbiertas) {
+    funtionDoors(puertasAbiertas);
+  }
+  window.onload = function () {
+    var panel = document.getElementById("panel");
+    panel.style.display = "flex";
+  };
+}
 // Obtén el contenedor del video y el elemento de video
 const videoContainer = document.getElementById("video-container");
 const videoElement = document.getElementById("video-element");
 const modalElement = document.getElementById("modal");
 const modalTextElement = document.getElementById("modalText");
+
 // Solicita acceso a la cámara del usuario
-const startCamera = () => {
-  // Cargar el modelo de detección de objetos
-  cocoSsd.load().then((model) => {
-    navigator.mediaDevices
-      .getUserMedia({ video: true })
-      .then(function (stream) {
-        // Muestra el video en el elemento de video
 
-        videoElement.srcObject = stream;
-        videoElement.play();
+const startCamera = (cameraActivada) => {
+  if (cameraActivada) {
+    // Cargar el modelo de detección de objetos
+    cocoSsd.load().then((model) => {
+      navigator.mediaDevices
+        .getUserMedia({ video: true })
+        .then(function (stream) {
+          // Muestra el video en el elemento de video
 
-        // Inserta el elemento de video dentro del contenedor
-        videoContainer.appendChild(videoElement);
-        cameraStatus = true;
-        setInterval(() => {
-          model.detect(videoElement).then((predictions) => {
-            console.log(predictions);
-            // Verificar si se detectó un perro o un humano
-            const hasDog = predictions.some(
-              (prediction) => prediction.class === "dog"
-            );
-            const hasPerson = predictions.some(
-              (prediction) => prediction.class === "person"
-            );
+          videoElement.srcObject = stream;
+          videoElement.play();
 
-            // Mostrar el modal y reproducir la voz si se detecta un perro o un humano
-            if (hasDog || hasPerson) {
-              modalTextElement.textContent = `${hasDog ? "Perro " : ""}${
-                hasDog && hasPerson ? "y " : ""
-              }${hasPerson ? "Humano" : ""} detectado`;
-              modalElement.style.display = "block";
-
-              // Reproducir voz
-              const utterance = new SpeechSynthesisUtterance(
-                modalTextElement.textContent
+          // Inserta el elemento de video dentro del contenedor
+          videoContainer.appendChild(videoElement);
+          cameraStatus = true;
+          setInterval(() => {
+            model.detect(videoElement).then((predictions) => {
+              console.log(predictions);
+              // Verificar si se detectó un perro o un humano
+              const hasDog = predictions.some(
+                (prediction) => prediction.class === "dog"
               );
-              speechSynthesis.speak(utterance);
-            } else {
-              modalElement.style.display = "none";
-            }
-          });
-        }, 5000);
-      })
-      .catch(function (error) {
-        console.error("Error al obtener acceso a la cámara:", error);
-      });
-  });
+              const hasPerson = predictions.some(
+                (prediction) => prediction.class === "person"
+              );
+
+              // Mostrar el modal y reproducir la voz si se detecta un perro o un humano
+              if (hasDog || hasPerson) {
+                modalTextElement.textContent = `${hasDog ? "Perro " : ""}${
+                  hasDog && hasPerson ? "y " : ""
+                }${hasPerson ? "Humano" : ""} detectado`;
+                modalElement.style.display = "block";
+
+                // Reproducir voz
+                const utterance = new SpeechSynthesisUtterance(
+                  modalTextElement.textContent
+                );
+                speechSynthesis.speak(utterance);
+              } else {
+                modalElement.style.display = "none";
+              }
+            });
+          }, 5000);
+        })
+        .catch(function (error) {
+          console.error("Error al obtener acceso a la cámara:", error);
+        });
+    });
+  }
 };
 
 const stopCamera = () => {
@@ -102,6 +145,7 @@ const stopCamera = () => {
     cameraStatus = false;
   }
 };
+
 // Request permission to use the microphone
 navigator.mediaDevices
   .getUserMedia({ audio: true })
@@ -255,36 +299,6 @@ const saveName = () => {
   }
 };
 
-function guardarConfiguracion() {
-  var nombreInput = document.getElementById("nombreInput");
-  var lucesCheckbox = document.getElementById("lucesCheckbox");
-  var puertasCheckbox = document.getElementById("puertasCheckbox");
-  var reconocimientoCheckbox = document.getElementById(
-    "reconocimientoCheckbox"
-  );
-
-  var nombreUsuario = nombreInput.value;
-  var lucesActivadas = lucesCheckbox.checked;
-  var puertasAbiertas = puertasCheckbox.checked;
-  var reconocimientoActivado = reconocimientoCheckbox.checked;
-
-  // Aquí puedes hacer lo que necesites con las configuraciones guardadas y el nombre del usuario
-
-  var panel = document.getElementById("panel");
-  panel.style.display = "none";
-
-  // Reproducir el nombre del usuario en forma de mensaje de voz
-  var mensajeVoz = new SpeechSynthesisUtterance();
-  mensajeVoz.text =
-    "Hola " + nombreUsuario + ", tu configuración ha sido guardada.";
-  speechSynthesis.speak(mensajeVoz);
-}
-
-window.onload = function () {
-  var panel = document.getElementById("panel");
-  panel.style.display = "flex";
-};
-
 const toggleCamera = () => {
   if (cameraStatus) {
     stopCamera();
@@ -293,15 +307,20 @@ const toggleCamera = () => {
   }
 };
 
-const toggleLight = (roomId) => {
-  if (lucesActivadas === checked) {
-    rooms[roomId].classList.toggle("lights-on");
-  } else {
+let funtionLight = (lucesActivadas) => {
+  if (lucesActivadas) {
+    toggleLight = (roomId) => {
+      rooms[roomId].classList.toggle("lights-on");
+    };
   }
 };
 
-const toggleDoorsAndWindows = (roomId) => {
-  doorsAndWindows[roomId].classList.toggle("open");
+let funtionDoors = (puertasAbiertas) => {
+  if (puertasAbiertas) {
+    toggleDoorsAndWindows = (roomId) => {
+      doorsAndWindows[roomId].classList.toggle("open");
+    };
+  }
 };
 
 const hideModal = () => {
